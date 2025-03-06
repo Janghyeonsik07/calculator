c9841cae56b8c94e199c6281054f856a983eff62
developer1 <dev1@email.com>
2025-03-06 12:47:14 +0900

    Add text edit te1 and fix btn1 handler function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # ch 4.2.1 main.py
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox)
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit)
 from PyQt5.QtGui import QIcon # icon 추가
 
 class Calculator(QWidget):
@@ -8,15 +8,16 @@ class Calculator(QWidget):
         super().__init__()
         self.initUI()
 
-    def activateMessage(self):
-        QMessageBox.information(self, 'information', 'Button clicked!')
 
     def initUI(self):
+        self.te1=QPlainTextEdit()
+        self.te1.setReadOnly(True)
+
         self.btn1 = QPushButton('Message', self)
         self.btn1.clicked.connect(self.activateMessage)
 
         vbox = QVBoxLayout()
-        vbox.addStretch(1)
+        vbox.addWidget(self.te1)
         vbox.addWidget(self.btn1)
         vbox.addStretch(1)
 
@@ -26,6 +27,10 @@ class Calculator(QWidget):
         self.setWindowIcon(QIcon('Calc.png'))
         self.resize(250,250)
         self.show()
+
+    def activateMessage(self):
+        QMessageBox.information(self, 'information', 'Button clicked!')
+        self.te1.appendPlainText('Button clicked!')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
